66aa97050c3454c147ea7daa2bb6221cb084d875
vincentwangzx <vincent.nicodemus@gmail.com>
2021-01-03 22:11:22 +0800

    added 2 functions to the app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,39 +1,54 @@
 import React, {useState} from 'react'; 
 import Todoform from './Todoform';
 import { VscClose } from 'react-icons/vsc';
-import { AiOutlineEdit } from 'react-icons/ai';
+import { AiOutlineCheck, AiOutlineEdit, AiOutlineExclamation } from 'react-icons/ai';
 
-function TodoItem({ items, completeItem, removeItem, updateItem }) {
+function TodoItem({ items, completeItem, removeItem, updateItem, colorItem }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     });
 
+
     const submitUpdate = value => {
+
         updateItem(edit.id, value);
         setEdit({
             id: null,
             value:''
         });
     };
+    const dragStart = e => {
+        const target = e.target;
+
+        e.dataTransfer.setData('item_id', target.id);
+
+    };
+
+    const dragOver = e => {
+        e.stopPropagation();
+    }; 
 
     if (edit.id) {
         return <Todoform edit={edit} onSubmit={submitUpdate} />;
     }
 
     return items.map((item, index) => (
-        <div className={item.isComplete ? 'item-row complete' : 'item-row'}
-         key={index}>
+        <div className={item.isComplete ? 'item-row complete' : 'item-row'} id={item.isColored ? 'item-row-colored' : 'item-row'}
+         key={index} draggable="true" onDragStart={dragStart} onDragover={dragOver}> 
              <div key={item.id} onClick={() => completeItem(item.id)}>
-                 {item.text}
+                 {item.text} 
              </div>
              <div className="icons">
+                 <AiOutlineCheck
+                 onClick={() => completeItem(item.id)}
+                 />
                  <VscClose 
                  onClick={() => removeItem(item.id)}
-                 className='delete-icon'
                  />
                  <AiOutlineEdit onClick={() => setEdit({ id: item.id, value: item.text})}
-                 className='edit-icon'/>
+                 />
+                 <AiOutlineExclamation onClick={() => colorItem(item.id) } />
              </div>
          </div>
     ));
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -15,6 +15,8 @@ function Todolist() {
         setItem(newItem);
     };
 
+
+
     const updateItem = (itemId, newInput) => {
         if(!newInput.text || /^\s*$/.test(newInput.text)) {
             return;
@@ -40,11 +42,39 @@ function Todolist() {
         setItem(updatedItem);
     }
 
+    
+    const colorItem = id => {
+        let updatedItem = items.map(item => {
+            if (item.id === id) {
+                item.isColored = !item.isColored
+            } 
+            return item;
+        });
+        setItem(updatedItem);
+    }
+
+    const drop = e => {
+        e.preventDefault();
+        const item_id = e.dataTransfer.getData('item_id')
+
+        const item = document.getElementById(item_id);
+        item.style.display = 'block';
+
+        e.target.appendChild(item);
+    }
+
+    const dragOver = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Let's start to plan your day! </h1>
+        <div
+            onDrop={drop}
+            onDragover={dragOver}
+        >
+            <h1>Take charge of your today</h1>
             <Todoform onSubmit={addTodo} />
-            <TodoItem items={items} completeItem={completeItem} updateItem={updateItem} removeItem={removeItem}/>
+            <TodoItem items={items} completeItem={completeItem} updateItem={updateItem} removeItem={removeItem} colorItem={colorItem}/>
         </div>
     )
 }
